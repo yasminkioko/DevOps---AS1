77699ae43f95da4b9ae21b830c79e5ac378102cb
Yasmin Kioko <yasmin_kioko@hotmail.com>
2025-09-15 20:13:06 -0300

    update

--- calculadora.py
+++ calculadora.py
@@ -1,40 +1,33 @@
-import sys
+import math
 
-def sum(self, x,y):
-    return x+y
+class operations:
 
-def subtraction(self, x,y):
-    return x-y
+    def sum(self, x,y):
+        return x+y
 
-def multiplication(self, x,y):
-    return x*y
+    def subtraction(self, x,y):
+        return x-y
 
-def division(self, x,y):
-    return x/y if y != 0 else None
+    def muntiplication(self, x,y):
+        return x*y
 
-def main():
-    if len(sys.argv) != 4:
-        print("python3 calculadora.py <operação> <número1> <número2>")
-        return
+    def division(self, x,y):
+        return x/y if y != 0 else None
+    
+    def pow(x,y):
+        return math.pow(x,y)
 
-    operacao = sys.argv[1]
-    num1 = float(sys.argv[2])
-    num2 = float(sys.argv[3])
-
-    if operacao == '1':
-        print("Result:", sum(num1, num2))
-
-    elif operacao == '2':
-        print("Result:", subtraction(num1, num2))
-
-    elif operacao == '3':
-        print("Result:", multiplication(num1, num2))
-
-    elif operacao == '4':
-        print("Result:", division(num1, num2))
-        
-    else:
-        print("Invalid")
-
-if __name__ == "__main__":
-    main()
+    def sqrt(x):
+        return math.sqrt(x)
+    
+    def acos(x):
+        return math.acos(x)
+    
+    def asen(x):
+        return math.asin(x)
+    
+    def atan(x):
+        return math.atan(x)
+    
+    def atan2(x,y):
+        return math.atan2(x,y)
